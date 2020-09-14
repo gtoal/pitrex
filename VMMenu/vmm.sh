1e9003a1e45ddfbe6c651973510503ef555ffd4a
Graham Toal <gtoal@gtoal.com>
2020-09-13 22:28:51 -0500

    Adding Vectrex examples to VMMenu

--- VMMenu/vmm.sh
+++ VMMenu/vmm.sh
@@ -7,8 +7,11 @@ game="${1,,}"
 if [[ $game == *.vec ]] || [[ $game == *.gam ]] || [[ $game == *.bin ]]
 then
    echo "Vectrex Game Selected"
-   echo "Command Line: /usr/local/bin/advmess vectrex -cart \"$1\""
-   /usr/local/bin/advmess vectrex -cart "$1"
+   echo "Command Line: ../vecx.emulated/vecx \"$1\""
+   # /usr/local/bin/advmess vectrex -cart "$1"
+   pushd ../vecx.emulated
+   ./vecx "$1"
+   popd
 else
     echo "Mame Game Selected"
     echo "Command Line: /usr/local/bin/advmame $1 $2"
